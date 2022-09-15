4b18d11dab703583305213dc36d2734321906980
Devyanshi <devyanshi2106@gmail.com>
2022-09-15 20:45:28 +0530

    Added more pattern codes

--- Patterns/Main.java
+++ Patterns/Main.java
@@ -11,8 +11,9 @@ public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.print("Enter the number of lines: ");
     int n = sc.nextInt();
-    pattern1(n);
+    pattern4(n);
 }
+
 public static void pattern1(int n){
     int i,j;
         for(i=1;i<=5;i++)
@@ -24,4 +25,74 @@ public static void pattern1(int n){
             System.out.println("");
 }
 }
+// ****
+// ***
+// **
+// *
+
+
+ public static void pattern2(int n)
+{
+    int i,j;
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=i;j++)
+        {
+             System.out.print("*");
+        }
+
+        System.out.print("\n");
+    }
+}
+
+
+//    *
+//   **
+//  ***
+// ****
+public static void pattern3(int n)
+{
+    int i,j;
+    for(i=1;i<=n;i++)
+    {
+        for(j=n;j>=1;j--)
+        {
+            if(j<=i)
+            {
+          System.out.print("*");
+            }        
+             else
+             {
+                 System.out.print(" ");
+             }
+            }
+
+        System.out.print("\n");
+    }
+}
+
+// ****
+//  ***
+//   **
+//    *
+
+public static void pattern4(int n)
+{
+    int i,j;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(j>=i)
+            {
+          System.out.print("*");
+            }        
+             else
+             {
+                 System.out.print(" ");
+             }
+            }
+            System.out.println("");
+        }
+    }
 }
